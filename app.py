6a9d384bcc16e600425ce4b06d13ce402d79d4d7
Dany CM <danycmwow@gmail.com>
2022-05-13 19:49:25 +0200

    feat: Add a map with ~20km radius, 2 new slides and format code

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 from flask import Flask, render_template
-
+import folium
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    folium_resize_map = folium.Figure(width=500, height=537)
+    location = 49.16608033066788, 2.244747752531686
+
+    folium_map = folium.Map(
+        location,
+        tiles="https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}",
+        attr="Google Maps",
+        zoom_start=10,
+    ).add_to(folium_resize_map)
+    
+    folium.Circle(location, radius=float(40 * 370)).add_to(folium_map)
+
+    return render_template("index.html", map=folium_map._repr_html_())
 
 
 if __name__ == "__main__":
