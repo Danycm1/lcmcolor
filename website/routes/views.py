0ae6ab0de397a16ae9fa1ac4fdc72a4e5818c565
Dany CM <danycmwow@gmail.com>
2022-05-16 11:53:15 +0200

    feat: Remove not needed feature

--- website/routes/views.py
+++ website/routes/views.py
@@ -1,8 +1,6 @@
 import folium
-from flask import Blueprint, flash, redirect, render_template, request, url_for
-from flask_login import current_user, login_required, login_user, logout_user
-from website.database.models import Price, Schedule, User
-from werkzeug.security import check_password_hash
+from flask import Blueprint, render_template
+from website.database.models import Price, Schedule
 
 views = Blueprint("views", __name__)
 
@@ -27,28 +25,3 @@ def home():
         pricedb=Price.query.all(),
         scheduledb=Schedule.query.all(),
     )
-
-
-@views.route("/logout")
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for("views.login"))
-
-
-@views.route("/login", methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        username = request.form.get("username")
-        password = request.form.get("password")
-
-        user = User.query.filter_by(username=username).first()
-
-        if check_password_hash(user.password, password):
-            login_user(user, remember=True)
-            print("dddd")
-            return redirect(url_for("views.home"))
-        else:
-            flash('Le nom d\'utilisateur ou le mot de passe est incorrect.')
-
-    return render_template("login.html", user=current_user)
